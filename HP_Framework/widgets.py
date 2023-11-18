626310a15687e3d50313af0b38ce45c3b5f248e1
MUHAMMAD FIRDAUS BIN ASRAR <86334510+LemanYkb@users.noreply.github.com>
2023-11-19 02:36:09 +0800

    Branching Preparation
    - placed course_engine, html_parsers, kulliyah_engine and models in seperate directory (Prereg_Engines)
    - made base template for different pages, components and subcomponents

--- HP_Framework/widgets.py
+++ HP_Framework/widgets.py
@@ -3,59 +3,6 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QOpenGLWidget
 from PyQt5.QtCore import Qt, QRect
 
-class HpGlWidget(QOpenGLWidget):
-    def __init__(self, parent=None):
-        QOpenGLWidget.__init__(self, parent)
-        self.setMinimumSize(640, 480)
-        self.rects = []
-        self.texts = []
-
-    def initializeGL(self):
-        glDisable(GL_DEPTH_TEST)
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-    def resizeGL(self, w, h):
-        glViewport(0, 0, w, h)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0, w, h, 0, -1, 1)  # Flip the y-axis to position the origin at the top-left corner
-        glMatrixMode(GL_MODELVIEW)
-
-    def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT)
-        
-        for rect in self.rects:
-            self.renderRect(rect.x, rect.y, rect.w, rect.h, rect.color)
-
-        painter = QtGui.QPainter(self)
-        for text in self.texts:
-            self.renderText(text.x, text.y, text.val, text.font, text.color, painter)
-
-    def renderRect(self, x, y, w, h, color):
-        glColor3f(color.r / 255, color.g / 255, color.b/ 255)
-        glBegin(GL_QUADS)
-        glVertex2f(x, y)
-        glVertex2f(x + w, y)
-        glVertex2f(x + w, y + h)
-        glVertex2f(x, y + h)
-        glEnd()
-
-    def renderText(self, x, y, text, font, color, painter : QtGui.QPainter):
-        painter.beginNativePainting()
-        painter.setPen(QtGui.QColor(color.r, color.g, color.b))
-        painter.setFont(QtGui.QFont(font.family, font.size, font.weight, font.italic))
-        font_metrics = painter.fontMetrics()
-        rect = font_metrics.boundingRect(QRect(), Qt.AlignmentFlag.AlignLeft, text)
-        painter.drawText(QRect(x, y, rect.width(), rect.height()), Qt.AlignmentFlag.AlignLeft, text)
-        painter.endNativePainting()
-
-    def addRect(self, x, y, w, h, color):
-        self.rects.append(HpGlRect(x, y, w, h, color))
-
-    def addText(self, x, y, text, font, color):
-        self.texts.append(HPGlText(x, y, text, font, color))
-
 class HpObject:
     def __init__(self, x, y, color):
         self.x = x
@@ -84,15 +31,21 @@ class HpGlRect(HpObject):
         self.setPosition(x, y)
         self.setDimension(w, h)
 
-class HPGlText:
-    def __init__(self, x, y, text, font, color):
+class HpGlText:
+    def __init__(self, x, y, text, font, color, painter : QtGui.QPainter):
         HpObject.__init__(self, x, y, color)
-        self.val = text
+        self.font = font
+        self.val = ''
         self.width = 0
         self.height = 0
-        self.font = font
+        self.setText(text, painter)
     
-    def setText(self, text):
+    def setText(self, text, painter : QtGui.QPainter):
+        painter.setFont(QtGui.QFont(self.font.family, self.font.size, self.font.weight, self.font.italic))
+        font_metrics = painter.fontMetrics()
+        rect = font_metrics.boundingRect(QRect(), Qt.AlignmentFlag.AlignLeft, text)
+        self.width = rect.width()
+        self.height = rect.height()
         self.val = text
 
     def setSize(self, size):
@@ -113,4 +66,54 @@ class HpFont:
         self.size = size
         self.weight = weight
         self.italic = italic
+
+class HpGlWidget(QOpenGLWidget):
+    def __init__(self, parent=None):
+        QOpenGLWidget.__init__(self, parent)
+        self.rects = []
+        self.texts = []
+
+    def initializeGL(self):
+        glDisable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    def resizeGL(self, w, h):
+        glViewport(0, 0, w, h)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, w, h, 0, -1, 1)  # Flip the y-axis to position the origin at the top-left corner
+        glMatrixMode(GL_MODELVIEW)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        
+        for rect in self.rects:
+            self.renderRect(rect)
+
+        painter = QtGui.QPainter(self)
+        for text in self.texts:
+            self.renderText(text, painter)
+
+    def renderRect(self, rect : HpGlRect):
+        glColor3f(rect.color.r / 255, rect.color.g / 255, rect.color.b/ 255)
+        glBegin(GL_QUADS)
+        glVertex2f(rect.x, rect.y)
+        glVertex2f(rect.x + rect.w, rect.y)
+        glVertex2f(rect.x + rect.w, rect.y + rect.h)
+        glVertex2f(rect.x, rect.y + rect.h)
+        glEnd()
+
+    def renderText(self, text : HpGlText, painter : QtGui.QPainter):
+        painter.beginNativePainting()
+        painter.setPen(QtGui.QColor(text.color.r, text.color.g, text.color.b))
+        painter.setFont(QtGui.QFont(text.font.family, text.font.size, text.font.weight, text.font.italic))
+        painter.drawText(QRect(text.x, text.y, text.width, text.height), Qt.AlignmentFlag.AlignLeft, text.val)
+        painter.endNativePainting()
+
+    def addRect(self, x, y, w, h, color):
+        self.rects.append(HpGlRect(x, y, w, h, color))
+
+    def addText(self, x, y, text, font, color):
+        self.texts.append(HpGlText(x, y, text, font, color, self))
     
